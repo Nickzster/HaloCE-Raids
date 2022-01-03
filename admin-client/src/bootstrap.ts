8e5a71f55d6042888baa21bb01dc507aa1656fb6
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2022-01-03 00:52:28 -0800

    added bootstrap items for tables

--- admin-client/src/bootstrap.ts
+++ admin-client/src/bootstrap.ts
@@ -1,5 +1,7 @@
 import FirebaseController from "./firebase";
 
+export type Tables = "players" | "tags" | "sessions" | "bosses" | "items";
+
 const BOOTSTRAP_DATA = {
   players: [
     {
@@ -22,6 +24,7 @@ const BOOTSTRAP_DATA = {
         "smg",
       ],
       player_code: "458715",
+      id: "Nickster",
     },
   ],
   sessions: [],
@@ -29,71 +32,90 @@ const BOOTSTRAP_DATA = {
   tags: [
     {
       key: "magnum",
+      id: "magnum",
       ref: "aerial\\weapons\\magnum\\magnum",
     },
     {
       key: "assault_rifle",
+      id: "assault_rifle",
       ref: "aerial\\weapons\\heavy assault rifle\\heavy assault rifle",
     },
     {
       key: "fuel_rod_gun",
+      id: "fuel_rod_gun",
       ref: "aerial\\weapons\\big fuelrod gun\\big fuelrod gun",
     },
     {
       key: "chaingun",
+      id: "chaingun",
       ref: "aerial\\weapons\\chaingun\\chaingun",
     },
     {
       key: "energy_sword",
+      id: "energy_sword",
       ref: "aerial\\weapons\\energy sword\\energy sword",
     },
     {
       key: "gravity_rifle",
+      id: "gravity_rifle",
       ref: "aerial\\weapons\\gravity rifle\\gravity rifle",
     },
     {
       key: "machete",
+      id: "machete",
       ref: "aerial\\weapons\\machete\\machete",
     },
     {
       key: "needler",
+      id: "needler",
       ref: "aerial\\weapons\\needler\\needler",
     },
     {
       key: "plasma_pistol",
+      id: "plasma_pistol",
       ref: "aerial\\weapons\\plasma pistol\\plasma pistol",
     },
     {
       key: "plasma_rifle",
+      id: "plasma_rifle",
       ref: "aerial\\weapons\\plasma rifle\\plasma rifle",
     },
     {
       key: "rocket_launcher",
+      id: "rocket_launcher",
       ref: "aerial\\weapons\\rocketlauncher\\rocketlauncher",
     },
     {
       key: "shotgun",
+      id: "shotgun",
       ref: "aerial\\weapons\\shotgun\\shotgun",
     },
     {
       key: "smg",
+      id: "smg",
       ref: "aerial\\weapons\\smg\\smg",
     },
     {
       key: "super_shotgun",
+      id: "super_shotgun",
       ref: "aerial\\weapons\\super shotgun\\super shotgun",
     },
   ],
   items: [],
 };
 
-export const bootstrap = async (
-  key: "players" | "tags" | "sessions" | "bosses" | "items"
-) => {
+export const bootstrap = async (key: Tables) => {
   try {
     const data = BOOTSTRAP_DATA[key];
     for (let i = 0; i < data.length; i++) {
-      await new FirebaseController(key).create(data[i]);
+      let recordToCreate = data[i];
+      if (recordToCreate.id) {
+        let id = recordToCreate.id;
+        recordToCreate.id = undefined;
+        await new FirebaseController(key).create(recordToCreate, id);
+        continue;
+      }
+      await new FirebaseController(key).create(recordToCreate);
     }
   } catch (err) {
     throw err;
