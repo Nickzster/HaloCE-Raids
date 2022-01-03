2ee9e8c83c9dfa63054339b7006be69c7743e3d5
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2022-01-03 00:48:40 -0800

    Updated firebase controller

--- admin-client/src/firebase/index.ts
+++ admin-client/src/firebase/index.ts
@@ -1,29 +1,38 @@
 import FirebaseDriver from "./driver";
 import { v4 as uuidv4 } from "uuid";
+import { Tables } from "../bootstrap";
 
 class FirebaseController {
   private db: FirebaseFirestore.Firestore;
-  private collectionName: string;
-  public constructor(collectionName: string) {
+  private collectionName: Tables;
+  public constructor(collectionName: Tables) {
     this.collectionName = collectionName;
     this.db = FirebaseDriver.initalize().getDB();
     return this;
   }
 
-  public async get(query = { id: null, ids: null }) {
-    const returnedItems: any[] = [];
-    if (query.id) {
-      // Get by ID
-    }
-    if (query.ids) {
-      //Get by multiple ID's
-    }
+  public async getAll() {
     // Get All
+    const returnedItems: any[] = [];
     const items = await this.db.collection(this.collectionName).get();
     items.forEach((item) =>
       returnedItems.push({ item: item.data(), id: item.id })
     );
     return returnedItems;
+  }
+
+  public async get(query = { id: null, ids: null }) {
+    const fbRef = this.db.collection(this.collectionName);
+    if (query.id) {
+      // Get by ID
+      const item = await (await fbRef.doc(query.id).get()).data();
+      return item;
+    }
+    if (query.ids) {
+      //Get by multiple ID's
+    }
+
+    return null;
   }
 
   public async create(recordToCreate: any, id = uuidv4()) {
@@ -37,7 +46,34 @@ class FirebaseController {
     }
   }
 
-  public async update() {}
+  public async update(id, updatedRecord) {
+    try {
+      if (this.collectionName === "players") {
+        const recordToUpdate = await this.get({ id, ids: null });
+
+        // Need to scan through input to ONLY update deltas
+        let changes = 0;
+        for (let key in recordToUpdate) {
+          if (updatedRecord[key]) {
+            changes++;
+            recordToUpdate[key] = updatedRecord[key];
+          }
+        }
+
+        if (changes === 0) return null;
+
+        const response = await this.db
+          .collection(this.collectionName)
+          .doc(id)
+          .set(recordToUpdate);
+        return response;
+      }
+
+      return null;
+    } catch (err) {
+      throw err;
+    }
+  }
 
   public async delete() {}
 }
