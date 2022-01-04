37a260c32c178c6525bb602483548222fb4a0c07
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2022-01-04 00:38:14 -0800

    Decoupled main app logic to module for reuseability in tests

--- webapp/server/src/app.ts
+++ webapp/server/src/app.ts
@@ -1,19 +1,4 @@
-import express from "express";
-import cors from "cors";
-import { Players, Items, Tags, Bosses, Events } from "./routes";
+import { configureApp, startApp } from "./util/App";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use("/events", Events);
-app.use("/players", Players);
-app.use("/items", Items);
-app.use("/tags", Tags);
-app.use("/bosses", Bosses);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () =>
-  console.log(`Halo CE: Raids server listening on port ${PORT}`)
-);
+configureApp();
+startApp();
